4a747d8cdf2f7b4ac0d625fa7c8aba2838259a3e
Matthew Harwood <harwood@uber.com>
2019-08-13 15:24:01 -0700

    added working types.

---
+++ src/components/hello.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Hello = () => (
+  <div>hello</div>
+);
+
+export default Hello;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,15 +5,15 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import { styled } from "styletron-react";
+import Hello from '../components/hello';
 const Colored = styled("div", { color: "blue" });
-
 
 
 const x: string = '1';
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-
+    <Hello />
   < p > Welcome to your new Gatsby site. { x }</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
