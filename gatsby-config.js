88a35e824cd23a88e886c6e625e59d3dd546ea64
Matthew Harwood <harwood@uber.com>
2019-08-13 11:50:23 -0700

    br

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,13 +19,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-netlify`,
       options: {
-        headers: { "/*.br": ["content-encoding: br"]}, // option to add more headers. `Link` headers are transformed by the below criteria
-        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
-        mergeSecurityHeaders: true, // boolean to turn off the default security headers
-        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
-        mergeCachingHeaders: true, // boolean to turn off the default caching headers
-        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
-        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+        headers: { "/*.br": ["content-encoding: br"]},
       },
     },
     `gatsby-plugin-react-helmet`,
@@ -53,6 +47,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
