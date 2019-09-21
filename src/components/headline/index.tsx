d9057c03b3e72e1fb3ec8133fd20eabcaf5edf29
Matthew Harwood <harwood@uber.com>
2019-09-21 08:18:48 -0700

    updated components

---
+++ src/components/headline/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Display1, Display3, Display4 } from 'baseui/typography';
+
+const Headline = (props: any) => {
+  return (
+    <>
+      <Display4 styles={{ color: "red" }}>
+        {props.eyebrow.text}
+      </Display4>
+      <Display1>{props.main.text}</Display1>
+      <Display3>{props.sub.text}</Display3>
+    </>
+  )
+}
+
+export {
+  Headline
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import React from "react"
-
-import { styled } from "styletron-react";
-import { Button } from "baseui/button";
-
+import { Headline } from '../components/headline';
 
 const IndexPage = () => (
   <>
-    <Button onClick={() => alert("click")}>Hello</Button>
+    <Headline
+      main={{ text: 'Nike Jordan Editor' }}
+      sub={{text: 'A real-time WYSIWYG editor for project owners, designers, copywriters to rapid prototype pages.'}}
+      eyebrow={{ text: 'Web Application', textColor: 'red' }} />
   </>
 )
 
