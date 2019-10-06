e284f01e469751e09cec668aca16548a59010011
Matthew Harwood <harwood@uber.com>
2019-10-05 17:38:13 -0700

    added highlight block

---
+++ src/components/highlight/index.tsx
@@ -0,0 +1,130 @@
+import React from 'react';
+import { Section, Container, Grid } from '../grid-system';
+import { useStyletron } from 'baseui';
+import { Display4, Label1, Label4, Paragraph1 } from 'baseui/typography';
+import Img from 'gatsby-image';
+import { type } from 'os';
+const GridItemLeft = ({ leftCols, orderLeft, children }: any) => {
+  const [css, theme] = useStyletron();
+  const c = {
+    className: css({
+      order: orderLeft,
+      display: 'grid',
+      gridColumn: `span ${leftCols}`,
+    }),
+  };
+
+  return <div {...c}>{children}</div>;
+};
+
+const GridItemRight = ({ rightCols, orderRight, children }: any) => {
+  const [css, theme] = useStyletron();
+  const c = {
+    className: css({
+      order: orderRight,
+      display: 'grid',
+      gridColumn: `span ${rightCols}`,
+    }),
+  };
+
+  return <div {...c}>{children}</div>;
+};
+const contentTypeLeft = (
+  type: string,
+  accent: string,
+  eyebrow: string,
+  title: string,
+  body: string,
+  src: string
+) => {
+  if (type === 'text') {
+    return (
+      <>
+        <Label1 color={accent}>{eyebrow}</Label1>
+        <Display4 color={'inheirt'}>{title}</Display4>
+        <Paragraph1 color={'inheirt'}>{body}</Paragraph1>
+      </>
+    );
+  } else {
+    return (
+      <Img
+        style={{ boxShadow: 'inset 0 0 0 1000px hsla(0, 0%, 0%, 0.04)' }}
+        fluid={src}
+      />
+    );
+  }
+};
+
+const contentTypeRight = (
+  type: string,
+  accent: string,
+  eyebrow: string,
+  title: string,
+  body: string,
+  src: string
+) => {
+  if (type === 'img') {
+    return (
+      <>
+        <Label1 color={accent}>{eyebrow}</Label1>
+        <Display4 color={'inheirt'}>{title}</Display4>
+        <Paragraph1 color={'inheirt'}>{body}</Paragraph1>
+      </>
+    );
+  } else {
+    return (
+      <Img
+        style={{ boxShadow: 'inset 0 0 0 1000px hsla(0, 0%, 0%, 0.04)' }}
+        fluid={src}
+      />
+    );
+  }
+};
+const Highlight = ({
+  $backgroundColor,
+  $color,
+  $accentColor,
+  $borderBottom = false,
+  eyebrow = 'eyebrow',
+  title = 'title',
+  body = 'body',
+  leftCols = 4,
+  rightCols = 8,
+  leftType = 'img',
+  rightType = 'text',
+  orderLeft = 0,
+  orderRight = 1,
+  src = 'https://images.unsplash.com/photo-1556912172-45b7abe8b7e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
+}: any) => {
+  const [, theme] = useStyletron();
+  const backgroundColor = $backgroundColor || theme.colors.primary100;
+  const color = $color || theme.colors.primary700;
+  const accent = $accentColor || theme.colors.accent;
+  return (
+    <Section
+      $backgroundColor={backgroundColor}
+      $color={color}
+      $collapsePaddingBottom={!!$borderBottom}
+    >
+      <Container $borderBottom={$borderBottom} $color={color}>
+        <Grid>
+          <GridItemLeft leftCols={leftCols} orderLeft={orderLeft}>
+            <>
+              <Label1 color={accent}>{eyebrow}</Label1>
+              <Display4 color={'inheirt'}>{title}</Display4>
+              <Paragraph1 color={'inheirt'}>{body}</Paragraph1>
+            </>
+          </GridItemLeft>
+          <GridItemRight rightCols={rightCols} orderRight={orderRight}>
+            <Img
+              style={{ boxShadow: 'inset 0 0 0 1000px hsla(0, 0%, 0%, 0.04)' }}
+              fluid={src}
+            />
+          </GridItemRight>
+        </Grid>
+      </Container>
+    </Section>
+  );
+};
+
+export { Highlight };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { PostHeader } from '../components/post-header';
 import { Summary } from '../components/summary';
 import { Process } from '../components/process';
-
+import { Highlight } from '../components/highlight';
+console.log('**'.repeat(100), Highlight);
 const IndexPage = () => (
   <>
     <PostHeader
@@ -48,7 +49,7 @@ const IndexPage = () => (
       }}
     />
     <Summary
-      eyebrow={'01'}
+      eyebrow={'00'}
       title={'Summary'}
       body={
         'The agency AKQA and their client Nike Jordan worked together to complete dozens of champaign webpages. Nike ran the majority of the creative process with their in-house copywriters, photographers, and creative directors. AKQA was the production muscle, bring together all the creative assets into a www.nike.com hosted webpage. Both Nike and AKQA did great work together; however, AKQA’s goal was to have more creative ownership. Unfortunatly for AKQA this ownership was seemingly impossible. The process between AKQA and Nike was fragemented, we were given hard deadlines, short production times, and low budgets. All of which, created a bottleneck that essentially blocked any progress towards more creative ownership. We had a mission to fix this fragemented process by bridging the the communication gap between Client Nike and Agency AKQA.'
@@ -56,6 +57,43 @@ const IndexPage = () => (
       $borderBottom={true}
     />
     <Process />
+    <Highlight
+      eyebrow={'01'}
+      title={'Getting Started'}
+      leftCols={4}
+      rightCols={8}
+      orderLeft={0}
+      orderRight={1}
+      src={
+        'https://images.unsplash.com/photo-1556912172-45b7abe8b7e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80'
+      }
+      body={
+        'Stanley Yeung, a former AKQA designer assigned to Nike Jordan was leaving the company.  Stanley managed the production design work for webpages across Nike Jordan.  Over lunch, Stanley’s creative director jokingly said, “With Stanley leaving, I wish we could create a robot that could continue all his production design work.” We knew we couldn’t make a robot but we saw a potential design challenge.'
+      }
+    />
+    <Highlight
+      eyebrow={'02'}
+      title={'Identifing Pain Points.'}
+      leftCols={8}
+      rightCols={4}
+      orderLeft={1}
+      orderRight={0}
+      src={
+        'https://images.unsplash.com/photo-1556912172-45b7abe8b7e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80'
+      }
+      body={`Our design prompt was but a utterance over lunch about replacing a leaving employee with a robot.  We were unsure of what we were out to solve. Regardless, we put on our designers hats and went to best resource we knew at the time IDEO’s Method Cards. We better shaped our problem by journey mapping the process between AKQA and Nike 2014’s March Madness Campaign. Our journey map reveal that the process had many pain points:
+
+        - Launches were often tied to an event and therefore deadlines were non-negotiable.
+        - Creative handoffs often bleed into execution timelines.
+        - Expensive production cost in both design and technology departements.
+        - Physical distance between AKQA and Nike caused long feedback turn arounds.
+        - AKQA Designers used Photoshop which didn’t offer a good solution for Responsive design.
+        - Nike’s CMS CQ5 was slow and required a certified trained Authorer to use it.
+
+         These pain points were used to help framed the product requirements.
+
+        `}
+    />
   </>
 );
 
