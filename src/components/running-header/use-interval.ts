178992caaa8bc7e6b107551e8d7bb6fdaa7b019c
Matthew Harwood <harwood@uber.com>
2019-10-17 12:37:11 -0700

    refactor of running header.

---
+++ src/components/running-header/use-interval.ts
@@ -0,0 +1,23 @@
+import React, { useRef, useEffect } from 'react';
+
+export function useInterval(callback?: () => void, delay?: number) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    // @ts-ignore
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      // @ts-ignore
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
