441e45869209d4b8a4381641c993ab935ceb210b
sangeet <sangeet.khatri@protonmail.com>
2019-10-18 11:18:49 +0530

    Added styles to Home Page

--- src/components/folio/index.tsx
+++ src/components/folio/index.tsx
@@ -17,20 +17,42 @@ const Folio: FunctionComponent<FolioProps> = ({
   link,
 }) => {
   const [css, theme] = useStyletron();
+  const lg = theme.mq.lg;
   const a = css({
     background: 'none',
     border: 'none',
     paddingTop: theme.sizing.scale500,
     paddingLeft: 0,
     paddingRight: 0,
-    paddingBottom: theme.sizing.scale500,
   });
 
   return (
-    <footer>
-      <H6 color={'accent'}>{headline}</H6>
-      <Paragraph4>
-        <span>{subheadline}</span>
+    <footer
+      className={css({
+        display: 'none',
+        [lg]: {
+          display: 'block',
+          fontSize: theme.sizing.scale500,
+          borderLeft: `${theme.sizing.scale100} solid tomato`,
+          paddingLeft: theme.sizing.scale700,
+          transform: 'rotate(-90deg)',
+          transformOrigin: 'left bottom',
+          position:'fixed',
+          bottom: 0,
+          left: 0,
+          marginLeft: theme.sizing.scale3200,
+        }
+      })}
+    >
+      <span className={css({ color: 'tomato' })}>{headline}</span>
+      <Paragraph4 margin={0}>
+        <span
+          className={css({
+            color: theme.colors.primary600,
+          })}
+        >
+          {subheadline}
+        </span>
         <a className={a} href={link && link.href}>
           {' '}
           {link && link.text}
