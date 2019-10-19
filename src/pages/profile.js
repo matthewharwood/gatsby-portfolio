c8b412e42fb43ece3d6a61d3e09a012e10855021
sangeet <sangeet.khatri@protonmail.com>
2019-10-19 05:37:15 +0530

    Added profile page

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './styles/profile.scss';
 import { Navbar } from '../components/navbar';
+import { H1, H2, H4, Label1, Paragraph2, Paragraph3 } from 'baseui/typography';
+import portrait from '../images/me4-sq.jpg';
+import { Grid } from '../components/grid-system';
 
 import {
   awards,
@@ -12,55 +15,314 @@ import {
   socials,
   visited,
 } from '../content/profile/profile';
+import { useStyletron } from 'baseui';
+
+const ProfileSection = ({ children }) => {
+  const [css, theme] = useStyletron();
+  return (
+    <section
+      className={css({
+        gridColumn: 'span 4',
+      })}
+    >
+      {children}
+    </section>
+  );
+};
+
+const Experiences = () => {
+  return (
+    <>
+      <SectionHeading heading="Experiences" />
+      {experiences.map(experience => (
+        <ExperienceItem {...experience} />
+      ))}
+    </>
+  );
+};
+
+const Education = () => {
+  return (
+    <>
+      <SectionHeading heading="Education" />
+      {education.map(experience => (
+        <ExperienceItem {...experience} />
+      ))}
+    </>
+  );
+};
+
+const Awards = () => {
+  const [css, theme] = useStyletron();
+  return (
+    <>
+      <SectionHeading heading="Awards" />
+      {awards.map(award => (
+        <ExperienceItem {...award} />
+      ))}
+    </>
+  );
+};
+
+const SectionHeading = ({ heading = 'Heading' }) => {
+  const [css, theme] = useStyletron();
+  return <H2 marginBottom={theme.sizing.scale700}>{heading}</H2>;
+};
+
+const Technical = () => {
+  return (
+    <>
+      <SectionHeading heading="Technical" />
+      <Paragraph3>
+        {techniques.join(', ')}
+      </Paragraph3>
+    </>
+  );
+};
+
+const Interests = () => {
+  return (
+    <>
+      <SectionHeading heading="Interests" />
+      <Paragraph3 margin={0}>
+        {interests.join(', ')}
+      </Paragraph3>
+    </>
+  );
+};
+
+const Socials = () => {
+  const [css, theme] = useStyletron();
+  const dlChild = css({
+    margin: 0,
+    padding: 0,
+    width: '50%',
+  });
+  return (
+    <>
+      <SectionHeading heading="Socials" />
+      <div>
+        <dl
+          className={css({
+            display: theme.display.flex,
+            justifyContent: 'space-between',
+            flexWrap: 'wrap',
+          })}
+        >
+          {socials.map(({ place, url, text }) => (
+            <>
+              <dt className={dlChild}>
+                <Paragraph3 margin={0} marginBottom={theme.sizing.scale300}>
+                  {place}
+                </Paragraph3>
+              </dt>
+              <dd className={dlChild}>
+                <a
+                  href={url}
+                  className={css({
+                    color: theme.colors.accent,
+                    textDecoration: 'none',
+                  })}
+                >
+                  <Paragraph3
+                    margin={0}
+                    marginBottom={theme.sizing.scale300}
+                    color={'inherit'}
+                  >
+                    {text}
+                  </Paragraph3>
+                </a>
+              </dd>
+            </>
+          ))}
+        </dl>
+      </div>
+    </>
+  );
+};
+
+const RecentlyVisited = () => {
+  const [css, theme] = useStyletron();
+  return (
+    <>
+      <SectionHeading heading="Recently Visited" />
+      <ul
+        className={css({
+          listStyleType: 'none',
+          lineHeight: '1.5',
+          padding: 0,
+        })}
+      >
+        {visited.map(( { flag, countryName } ) => (
+          <li>{flag} {countryName}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+const ExperienceItem = ({
+  place = 'Company',
+  title = 'Senio Design Engineer',
+  dateRange= 'December 2018 ~ July 2019',
+  jobDescription = 'Works on building and maintaining uber.com',
+}) => {
+  const [css, theme] = useStyletron();
+  return (
+    <div
+      className={css({
+        paddingBottom: theme.sizing.scale1000,
+      })}
+    >
+      <Paragraph3 margin={0}>{place}</Paragraph3>
+      <Paragraph3
+        margin={0}
+        marginBottom={theme.sizing.scale400}
+        color={theme.colors.primary400}
+      >
+        {title}
+      </Paragraph3>
+      <Paragraph3 margin={0}>
+        {dateRange}
+      </Paragraph3>
+      <Paragraph3 margin={0}>{jobDescription}</Paragraph3>
+    </div>
+  );
+};
+
+const IntroSection = () => {
+  const [css, theme] = useStyletron();
+  return (
+    <section
+      className={css({
+        display: 'flex',
+        position: 'fixed',
+        top: 0,
+        width: '100vw',
+      })}
+    >
+      <header
+        className={css({
+          width: '50vw',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          padding: `0 ${theme.sizing.scale3200}`,
+        })}
+      >
+        <H1
+          margin={0}
+          marginBottom={theme.sizing.scale900}
+          className={css({ fontSize: theme.sizing.scale1400 })}
+        >
+          {me.name}
+        </H1>
+        <H4 margin={0} color={theme.colors.accent}>
+          {me.title}
+        </H4>
+        <Paragraph2>{me.description}</Paragraph2>
+      </header>
+      <img
+        src={portrait}
+        alt=""
+        className={css({
+          width: '50vw',
+          height: '100vh',
+          objectFit: 'cover',
+        })}
+      />
+    </section>
+  );
+};
+
+const ResumeHeader = () => {
+  const [css, theme] = useStyletron();
+  const aStyle = css({
+    textDecoration: 'none',
+    color: 'tomato',
+  });
+  return (
+      <section
+        className={css({
+          display: 'flex',
+          justifyContent: 'space-between',
+        })}
+      >
+        <header>
+          <H1 margin={0} className={css({ fontSize: theme.sizing.scale1400 })}>
+            Résumé
+          </H1>
+        </header>
+        <ul
+          className={css({
+            fontSize: theme.sizing.scale500,
+            lineHeight: theme.sizing.scale800,
+            listStyleType: 'none',
+            textAlign: 'right',
+          })}
+        >
+          <li>
+            <a className={aStyle} href={me.fullDomain}>
+              {me.domain}
+            </a>
+          </li>
+          <li>
+            <a
+              className={aStyle}
+              href={`mailto:${me.email}?subject=Hello Matthew`}
+            >
+              {me.email}
+            </a>
+          </li>
+          <li>
+            <a className={aStyle}>{me.phone}</a>
+          </li>
+        </ul>
+      </section>
+  )
+}
+
+const ResumeSection = () => {
+  const [css, theme] = useStyletron();
+  return (
+    <article
+      className={css({
+        position: 'relative',
+        background: theme.colors.primary50,
+        top: `calc(100vh - ${theme.sizing.scale4800})`,
+        margin: `0 ${theme.sizing.scale2400}`,
+        marginBottom: theme.sizing.scale2400,
+        padding: `${theme.sizing.scale3200} ${theme.sizing.scale1200}`,
+      })}
+    >
+      <ResumeHeader/>
+      <Grid>
+        <ProfileSection>
+          <Experiences />
+        </ProfileSection>
+        <ProfileSection>
+          <Education />
+          <Awards />
+        </ProfileSection>
+        <ProfileSection>
+          <Technical />
+          <Interests />
+          <Socials />
+          <RecentlyVisited />
+        </ProfileSection>
+      </Grid>
+    </article>
+  );
+};
 
 const Profile = () => {
+  const [css, theme] = useStyletron();
   return (
     <>
       <Navbar />
       <div>
-        <section className="section">
-          <header class="header">
-            <h2 class="h2">{me.name}</h2>
-            <h3 class="h3">{me.title}</h3>
-            <p class="p">{me.description}</p>
-          </header>
-          <div class="portrait"></div>
-        </section>
-        <article class="resume">
-          <header class="resumeHeader">
-            <h2 class="h2">Résumé</h2>
-          </header>
-          <section class="contact">
-            <ul class="ul">
-              <li>
-                <a class="a" href={me.fullDomain}>
-                  {me.domain}
-                </a>
-              </li>
-              <li>
-                <a class="a" href={`mailto:${me.email}?subject=Hello Matthew`}>
-                  {me.email}
-                </a>
-              </li>
-              <li>
-                <a class="a">{me.phone}</a>
-              </li>
-            </ul>
-          </section>
-          <section class="experience">
-            {/* <StandardBlock title={'Experiences'} data={experiences} /> */}
-          </section>
-          <section class="education">
-            {/* <StandardBlock title={'Education'} data={education} /> */}
-            {/* <StandardBlock title={'Awards'} data={awards} /> */}
-          </section>
-          <aside class="aside">
-            {/* <StandardList title={'Technical'} data={techniques} /> */}
-            {/* <StandardList title={'Interests'} data={interests} /> */}
-            {/* <StandardTable title={'Socials'} data={socials} /> */}
-            {/* <StandardVisitedList title={'Recently Visited'} data={visited} /> */}
-          </aside>
-        </article>
+        <IntroSection />
+        <ResumeSection />
       </div>
     </>
   );
