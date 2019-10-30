2cf9a7608bf0a5187b9e3ea620a85eb65742c090
sangeet <sangeet.khatri@protonmail.com>
2019-10-30 23:29:34 +0530

    Changed page creation query as well as template query from mdx to sanity

--- src/templates/workTemplate.tsx
+++ src/templates/workTemplate.tsx
@@ -1,35 +1,33 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import { MDXRenderer } from 'gatsby-plugin-mdx';
-
-
-
 
 import { Navbar } from '../components/navbar';
 import { Footer } from '../components/footer';
 import { PostPagination } from '../components/post-pagination';
 
 const WorkTemplate = ({ data, pageContext }: any) => {
+  const flattenedData = data.sanityWork;
   return (
     <>
       <Navbar />
-       <MDXRenderer>{data.mdx.body}</MDXRenderer>
-      <PostPagination
-        prevLink={{
-          link: pageContext.prev ? pageContext.prev.fields.slug : '',
-          title: pageContext.prev ? pageContext.prev.frontmatter.title : '',
-          description: pageContext.prev
-            ? pageContext.prev.frontmatter.description
-            : '',
-        }}
-        nextLink={{
-          link: pageContext.next ? pageContext.next.fields.slug : '',
-          title: pageContext.next ? pageContext.next.frontmatter.title : '',
-          description: pageContext.next
-            ? pageContext.next.frontmatter.description
-            : '',
-        }}
-      />
+      <pre>{JSON.stringify(flattenedData, null, 4)}</pre>
+      {/* <MDXRenderer>{data.mdx.body}</MDXRenderer> */}
+      {/* <PostPagination */}
+      {/*  prevLink={{ */}
+      {/*    link: pageContext.prev ? pageContext.prev.fields.slug : '', */}
+      {/*    title: pageContext.prev ? pageContext.prev.frontmatter.title : '', */}
+      {/*    description: pageContext.prev */}
+      {/*      ? pageContext.prev.frontmatter.description */}
+      {/*      : '', */}
+      {/*  }} */}
+      {/*  nextLink={{ */}
+      {/*    link: pageContext.next ? pageContext.next.fields.slug : '', */}
+      {/*    title: pageContext.next ? pageContext.next.frontmatter.title : '', */}
+      {/*    description: pageContext.next */}
+      {/*      ? pageContext.next.frontmatter.description */}
+      {/*      : '', */}
+      {/*  }} */}
+      {/* /> */}
       <Footer />
     </>
   );
@@ -37,14 +35,20 @@ const WorkTemplate = ({ data, pageContext }: any) => {
 
 export default WorkTemplate;
 
+// mdx(id: { eq: $id }) {
+//   id
+//   body
+//   frontmatter {
+//     title
+//   }
+// }
+
 export const pageQuery = graphql`
   query WorkPageQuery($id: String) {
-    mdx(id: { eq: $id }) {
+    sanityWork(id: { eq: $id }) {
       id
-      body
-      frontmatter {
-        title
-      }
+      title
+      _rawContent
     }
   }
 `;
