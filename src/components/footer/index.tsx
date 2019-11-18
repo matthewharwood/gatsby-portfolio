dcdc696a334fb21e57e993452c86c24cd468e1e6
Matthew Harwood <harwood@uber.com>
2019-11-17 21:25:58 -0800

    prettier.

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,35 +1,35 @@
-import React from "react"
-import { useStyletron } from "baseui"
-import { FooterLeft } from "./left"
-import { FooterRight } from "./right"
+import React from 'react';
+import { useStyletron } from 'baseui';
+import { FooterLeft } from './left';
+import { FooterRight } from './right';
 
-import { mq, display } from "../styles"
+import { mq, display } from '../styles';
 
 export const Footer = () => {
-  const [css, theme] = useStyletron()
-  const md = mq.md
-  const lg = mq.lg
+  const [css, theme] = useStyletron();
+  const md = mq.md;
+  const lg = mq.lg;
   return (
     <footer
       className={css({
         display: display.flex,
-        flexDirection: "column",
-        alignItems: "center",
+        flexDirection: 'column',
+        alignItems: 'center',
         paddingTop: theme.sizing.scale900,
         paddingBottom: theme.sizing.scale700,
         [md]: {
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
           paddingLeft: theme.sizing.scale800,
           paddingRight: theme.sizing.scale1000,
           paddingTop: theme.sizing.scale800,
           paddingBottom: theme.sizing.scale800,
         },
         [lg]: {
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
           paddingLeft: theme.sizing.scale800,
           paddingRight: theme.sizing.scale1000,
           paddingTop: theme.sizing.scale800,
@@ -37,12 +37,8 @@ export const Footer = () => {
         },
       })}
     >
-      <FooterLeft/>
-      <FooterRight/>
+      <FooterLeft />
+      <FooterRight />
     </footer>
-  )
-}
-
-
-
-
+  );
+};
