9c9a86c639c6b37eba253f2bf3bf4d6d48c44fd0
Matthew Harwood <harwood@uber.com>
2019-11-18 06:05:54 -0800

    started inputing static data for footer.

---
+++ src/components/footer/data.ts
@@ -0,0 +1,111 @@
+let SOCIAL = {
+  FACEBOOK: {
+    url: 'https://www.facebook.com/Watashi',
+  },
+  BUYMEACOFEE: {
+    url: 'https://www.buymeacoffee.com/XNGzQyR',
+  },
+  STACKOVERFLOW: {
+    url: 'https://stackoverflow.com/users/1191635/matthew-harwood',
+  },
+  CODESANDBOX: {
+    url: 'https://codesandbox.io/u/matthewharwood',
+  },
+  SNAPCHAT: {
+    url: 'https://www.snapchat.com/add/matthewharwood',
+  },
+  GOODREADS: {
+    url: 'https://www.goodreads.com/user/show/86210568-matthew-harwood',
+  },
+  MESSENGER: {
+    url: '',
+    src: 'https://imgur.com/a/DYP165R', // note need special handling,
+  },
+  DISCORD: {
+    url: 'Matty#9329',
+  },
+  ITCH: {
+    url: 'https://matthewharwood.itch.io/',
+  },
+  DEVTO: {
+    src: 'https://dev.to/matthewharwood',
+  },
+  MEDIUM: {
+    src: 'https://medium.com/@matty0187',
+  },
+  PAYPAL: {
+    src:
+      'https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=NCTR8C7UPNSZG&currency_code=USD&source=url',
+  },
+  STRAVA: {
+    src: 'https://www.strava.com/athletes/17972008',
+  },
+  REDDIT: {
+    src: 'https://www.reddit.com/user/matty0187',
+  },
+  KAGGLE: {
+    src: 'https://www.kaggle.com/matthewharwood',
+  },
+  EXERCISM: {
+    src: 'https://exercism.io/profiles/matthewharwood',
+  },
+  HACKERRANK: {
+    src: '',
+  },
+  AMAZON: {
+    src:
+      'https://www.amazon.com/gp/profile/amzn1.account.AFMSHBTARA37REFFWXQETAKS7N6Q',
+  },
+  STADIA: {
+    src: '',
+  },
+  STEAM: {
+    src: 'atreyuu',
+  },
+  VENMO: {
+    src: 'matty0187',
+  },
+  SKYPE: {
+    src: 'matty0187',
+  },
+  LINE: {
+    src: 'matty0187',
+  },
+  UBER: {
+    src: 'gk6pd',
+    description: 'Use this to sign up plz',
+  },
+  TWITCH: {
+    src: 'https://www.twitch.tv/matthewharwood',
+  },
+  LINKEDIN: {
+    src: 'https://www.linkedin.com/in/morningharwood/',
+  },
+  SWITCH: {
+    src: '',
+  },
+  YOUTUBE: {
+    src: 'https://www.youtube.com/user/Matty0187',
+  },
+  NPM: {
+    src: 'https://www.npmjs.com/~matthewharwood',
+  },
+  EMAIL: {
+    src: 'matthhar12@gmail.com',
+  },
+  GITHUB: {
+    src: 'https://github.com/matthewharwood',
+  },
+  TWITTER: {
+    src: 'https://twitter.com/Matty0187',
+  },
+  INSTAGRAM: {
+    src: 'https://www.instagram.com/matty0187/',
+  },
+  ETHEREUM: {
+    src:
+      'https://etherscan.io/address/0xb3d9dec692031257044a6f366ff4144997bb9205',
+  },
+};
+
+export { SOCIAL };
