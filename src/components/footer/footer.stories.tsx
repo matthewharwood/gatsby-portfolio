df6ceec47b74d3adc9bcc97204a04eb9eb2ae3e6
Matthew Harwood <harwood@uber.com>
2019-11-18 06:24:33 -0800

    remove footer fixtures.. unused.

--- src/components/footer/footer.stories.tsx
+++ src/components/footer/footer.stories.tsx
@@ -6,16 +6,16 @@ export default {
   title: 'footer',
 };
 
-const FIXTURE = {
-  headline: 'Good Morning Harwood',
-  subheadline: 'A creative playground by ',
-  link: {
-    href: 'https://uber.com',
-    text: 'Matthew Harwood',
-  },
-};
+// const FIXTURE = {
+//   headline: 'Good Morning Harwood',
+//   subheadline: 'A creative playground by ',
+//   link: {
+//     href: 'https://uber.com',
+//     text: 'Matthew Harwood',
+//   },
+// };
 
-export const toStorybook = () => <Footer {...FIXTURE} />;
+export const toStorybook = () => <Footer  />;
 
 toStorybook.story = {
   name: 'Footer Story',
