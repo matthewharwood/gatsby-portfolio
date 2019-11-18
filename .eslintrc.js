507f752d2b8be99a733b119334514f137c233dfc
Matthew Harwood <harwood@uber.com>
2019-11-18 06:20:47 -0800

    added bundle analyzer but only uncomment when testing locally.

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,8 @@ module.exports = {
   env: {
     jest: true,
     browser: true,
-    es6: true
+    es6: true,
+    node: true,
   },
   settings: {
     react: {
--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,12 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-eslint',
+    // {
+    //   resolve: 'gatsby-plugin-webpack-bundle-analyser-v2',
+    //   options: {
+    //     devMode: true,
+    //   },
+    // },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
