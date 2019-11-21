b3745d337753f75d55d195cc8035f931960fc047
sangeet <sangeet.khatri@protonmail.com>
2019-11-21 23:39:03 +0530

    Changing imageAsset schema to resolve breaking bug

--- sanity/schemas/shared/imageAsset.js
+++ sanity/schemas/shared/imageAsset.js
@@ -1,15 +1,17 @@
 export default {
   name: 'imageAsset',
   title: 'Image Asset',
-  type: 'image',
+  type: 'document',
   fields: [
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Image',
+    },
     {
       name: 'altText',
       type: 'string',
       title: 'Alt Text',
-      options: {
-        isHighlighted: true,
-      },
     },
   ],
 };
