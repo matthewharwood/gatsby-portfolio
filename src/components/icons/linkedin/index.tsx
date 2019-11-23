3a73336f4fe368346d1751f94e399d361a753eda
Matthew Harwood <harwood@uber.com>
2019-11-23 10:58:49 -0800

    fixed linkedin.

--- src/components/icons/linkedin/index.tsx
+++ src/components/icons/linkedin/index.tsx
@@ -4,6 +4,8 @@ export const LinkedinIcon = () => {
   return (
     <svg
       role="img"
+      width="24"
+      height="24"
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
     >
