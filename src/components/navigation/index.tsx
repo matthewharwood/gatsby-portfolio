32c43468dfa746d26a62850da15b22a14a81b014
matthewharwood <matthhar12@gmail.com>
2020-02-07 21:13:24 -0800

    commit

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useRef, MutableRefObject } from 'react';
 import {
   ALIGN,
-  StyledNavigationList,
   StyledNavigationItem,
+  StyledNavigationList,
   StyledRoot,
 } from 'baseui/header-navigation';
-import { Button, SHAPE, SIZE, KIND } from 'baseui/button';
+import { Button, KIND, SHAPE, SIZE } from 'baseui/button';
 import { useStyletron, withStyle } from 'baseui';
 
-
+import { Link, navigate } from 'gatsby';
 const HeaderNavigationRoot = withStyle(StyledRoot, ({ $theme }) => ({
   paddingLeft: 0,
   paddingRight: $theme.sizing.scale600,
@@ -32,8 +32,16 @@ const LogoText = () => {
     </h1>
   );
 };
+
 const Navigation = () => {
-  const navigationItems = ['WORK', 'LABS', 'PROFILE'];
+  const [css, theme] = useStyletron();
+  const classRef: MutableRefObject<any> = useRef();
+  const navigationItems = [
+    { name: 'WORK', route: '/work' },
+    { name: 'LABS', route: '/labs' },
+    { name: 'PROFILE', route: '/profile' },
+  ];
+  const activeClass = 'active';
   return (
     <HeaderNavigationRoot>
       <StyledNavigationList $align={ALIGN.left}>
@@ -45,14 +53,36 @@ const Navigation = () => {
       <StyledNavigationList $align={ALIGN.right}>
         {navigationItems.map((item, key) => (
           <StyledNavigationItem key={key}>
-            <Button
-              size={SIZE.default}
-              onClick={() => alert('click')}
-              shape={SHAPE.pill}
-              kind={KIND.primary}
+            <Link
+              ref={classRef}
+              className={css({
+                textDecoration: 'none',
+                color: 'inherit',
+              })}
+              activeClassName={activeClass}
+              to={item.route}
             >
-              {item}
-            </Button>
+              <Button
+                size={SIZE.default}
+                shape={SHAPE.pill}
+                kind={KIND.primary}
+                isSelected={
+                  classRef &&
+                  classRef.current &&
+                  classRef.current.classList &&
+                  classRef.current.classList.contains(activeClass)
+                }
+                overrides={{
+                  BaseButton: {
+                    style: {
+                      fontWeight: 'bold',
+                    },
+                  },
+                }}
+              >
+                {item.name}
+              </Button>
+            </Link>
           </StyledNavigationItem>
         ))}
       </StyledNavigationList>
